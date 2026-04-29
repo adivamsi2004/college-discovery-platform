dc0fbd40641a5f2e01d425e8096bedc90cd73f34
kohli <you@example.com>
2026-04-30 01:08:14 +0530

    fix: explicit vercel tracing for sqlite

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
+    outputFileTracingIncludes: {
+      '/api/**/*': ['./prisma/**/*'],
+    },
+  },
 };
 
 export default nextConfig;
